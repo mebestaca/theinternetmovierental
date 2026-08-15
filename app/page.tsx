c43e1f31f821fdaae4eaf7b12d7d497488df58a9
reinerjustin <reinerjustin.realica@edu.sait.ca>
2026-08-15 12:07:46 -0600

    edited page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
-import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main></main>
+    <>
+      <Navbar />
+
+      <main>
+        <h2>Welcome to Movie List</h2>
+      </main>
+
+    </>
   );
 }
